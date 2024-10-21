c3320fc550c35bb6f45fba2a5a4b679e506632f1
Amarthya K R <amarthya10@gmail.com>
2024-10-21 13:33:55 +0530

    [STYLES]: GRA logo added

--- superset/config.py
+++ superset/config.py
@@ -310,7 +310,7 @@ AUTH_RATE_LIMIT = "5 per second"
 APP_NAME = "Dashboards | India Urban Data Exchange"
 
 # Specify the App icon
-APP_ICON = "/static/assets/images/iudx-ideal.png"
+APP_ICON = "/static/assets/images/GRA.png"
 
 # Specify where clicking the logo would take the user'
 # Default value of None will take you to '/superset/welcome'
